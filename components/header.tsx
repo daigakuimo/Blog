bebc8178cfd8b80d42fa26a04dedd1ec060be8b2
daigakuimo <enaena128@gmail.com>
2022-09-17 15:11:37 +0900

    feat:first commit

---
+++ components/header.tsx
@@ -0,0 +1,11 @@
+import Logo from "./Logo";
+import Nav from "./nav";
+
+export default function Header() {
+    return (
+        <header>
+            <Logo />
+            <Nav />
+        </header>
+    )
+}
